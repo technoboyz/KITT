4c5c1675ccc70bbb3f27fe289f19d4a750145628
Alberto Mori <albi.mo87@hotmail.it>
2022-01-22 18:59:57 +0100

    models xml comments

--- src/KITT.Web.Models/Streamings/ScheduleStreamingModel.cs
+++ src/KITT.Web.Models/Streamings/ScheduleStreamingModel.cs
@@ -2,25 +2,49 @@
 
 namespace KITT.Web.Models.Streamings;
 
+/// <summary>
+/// Definisce le informazioni per la pianificazione di una nuova live
+/// </summary>
 public class ScheduleStreamingModel
 {
+    /// <summary>
+    /// Il titolo della live
+    /// </summary>
     [Required]
     public string Title { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Uno slug per la live
+    /// </summary>
     [Required]
     public string Slug { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Una data di pianificazione
+    /// </summary>
     [Required]
     public DateTime ScheduleDate { get; set; } = DateTime.Now;
 
+    /// <summary>
+    /// Un orario di inizio della diretta
+    /// </summary>
     [Required]
     public DateTime StartingTime { get; set; }
 
+    /// <summary>
+    /// Un orario di fine della diretta
+    /// </summary>
     [Required]
     public DateTime EndingTime { get; set; }
 
+    /// <summary>
+    /// Il canale in cui si svolgerà la diretta
+    /// </summary>
     [Required]
     public string HostingChannelUrl { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Un eventuale abstract per la diretta
+    /// </summary>
     public string? StreamingAbstract { get; set; }
 }
